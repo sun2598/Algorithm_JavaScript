2839ec28b9c498603ac51ce01a7b565ccaebf076
sun2598 <cksun2598@gmail.com>
2024-03-29 16:20:18 +0900

    24/03/29 uploading section05 questions

---
+++ section05_효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/01. 두 배열 합치기.js
@@ -0,0 +1,14 @@
+// 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+
+function solution(arr1, arr2) {
+    let answer = [];
+
+    answer = arr1.concat(arr2);
+    answer = answer.sort((a, b) => a - b);
+
+    return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution(a, b));
---
+++ section05_효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/08. 모든 아나그램(해쉬, 투포인터, 슬라이딩 윈도우).js
@@ -0,0 +1,15 @@
+// S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하 세요. 
+// 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+
+function compareMaps(map1, map2){
+
+}
+function solution(s, t){
+    let answer=0;
+
+    return answer;
+}
+
+let a="bacaAacba";
+let b="abc";
+console.log(solution(a, b));
