a29fd3357ceafb685f0fdd93affe15a06d5c7ace
sun2598 <cksun2598@gmail.com>
2024-04-08 13:20:06 +0900

    24/04/08 solved 06_01

--- section06_자료구조(스택,큐)/01. 올바른 괄호.js
+++ section06_자료구조(스택,큐)/01. 올바른 괄호.js
@@ -1,11 +1,25 @@
 // 괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
 // (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
 
-function solution(s){
-    let answer="YES";
+function solution(s) {
+    let answer = "YES";
 
+    stack = [];
+    for (let x of s) {
+        if (x === '(') {
+            stack.push(x);
+        } else { // ')'
+            if (stack.length === 0) {
+                return "NO";
+            }
+            stack.pop();
+        }
+    }
+    if (stack.length > 0) {
+        return "NO";
+    }
     return answer;
 }
 
-let a="(()(()))(()";
+let a = "(()(()))(()";
 console.log(solution(a));
