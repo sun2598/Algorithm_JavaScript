cc11a5f30bda34ec34902f51474a1fd094faff76
sun2598 <cksun2598@gmail.com>
2024-04-19 17:11:44 +0900

    24/04/19 solved 06_06

--- section06_자료구조(스택,큐)/06. 공주구하기.js
+++ section06_자료구조(스택,큐)/06. 공주구하기.js
@@ -1,7 +1,21 @@
-function solution(n, k){
-    let answer;
+function solution(n, k) {
+    let answer; // 마지막으로 남는 왕자의 번호를 저장할 변수
 
-    return answer;
+    // 초기 큐 설정: 1부터 n까지의 왕자 번호로 초기화
+    let queue = Array.from({ length: n }, (v, i) => i + 1);
+    while (queue.length) {
+        // k번째 왕자를 제외하기 전까지, 큐의 맨 앞 요소를 맨 뒤로 이동
+        for (let i = 1; i < k; i++) {
+            queue.push(queue.shift());
+        }
+        // k번째 왕자 제외
+        queue.shift();
+        // 큐에 남은 왕자가 한 명만 있으면, 그 왕자가 마지막으로 남는 왕자
+        if (queue.length === 1) {
+            answer = queue.shift(); // 마지막으로 남은 왕자를 answer에 할당
+        }
+    }
+    return answer; // 마지막으로 남은 왕자의 번호 반환
 }
 
-console.log(solution(8, 3));
+console.log(solution(8, 3)); // 7
