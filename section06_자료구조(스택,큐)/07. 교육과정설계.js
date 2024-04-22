e12365ad31ce8442c34673bb89feb1840097097c
sun2598 <cksun2598@gmail.com>
2024-04-22 18:50:41 +0900

    24/04/22 solving 06_07

--- section06_자료구조(스택,큐)/07. 교육과정설계.js
+++ section06_자료구조(스택,큐)/07. 교육과정설계.js
@@ -1,9 +1,12 @@
-function solution(need, plan){
-    let answer="YES";
+function solution(need, plan) {
+    let answer = "YES";
+
+    let queue = [];
+    
 
     return answer;
 }
 
-let a="CBA";
-let b="CBDAGE";
+let a = "CBA"; // 필수과목 순서
+let b = "CBDAGE"; // 현수가 설계한 필수과목 순서
 console.log(solution(a, b));
