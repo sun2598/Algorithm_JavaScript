068204acc3d286bb42a73bc8cc06cbc0a22d43c5
sun2598 <cksun2598@gmail.com>
2024-04-24 08:35:59 +0900

    24/04/24 uploading section07 questions

---
+++ section07_정렬과 그리디, 결정알고리즘(이분검색)/11. 뮤직비디오.js
@@ -0,0 +1,12 @@
+function count(songs, capacity) {
+
+}
+
+function solution(m, songs) {
+    let answer;
+
+    return answer;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
