dd192bb13cd34b45b3c42e77a85971a2f7669334
sun2598 <cksun2598@gmail.com>
2024-04-23 11:03:09 +0900

    24/04/23 solved 06_07

--- section06_자료구조(스택,큐)/07. 교육과정설계.js
+++ section06_자료구조(스택,큐)/07. 교육과정설계.js
@@ -1,8 +1,17 @@
 function solution(need, plan) {
     let answer = "YES";
 
-    let queue = [];
-    
+    let queue = need.split(''); // ['C', 'B', 'A']
+    for (let x of plan) {
+        if (queue.includes(x)) { // 현재 수업이 필수 과목 리스트에 포함되어 있는지 확인
+            if (x !== queue.shift()) {
+                return "NO"; // 포함되어 있다면 맨 앞 순서가 맞는지 확인
+            }
+        }
+    }
+    if (queue.length > 0) {
+        return "NO";
+    }
 
     return answer;
 }
