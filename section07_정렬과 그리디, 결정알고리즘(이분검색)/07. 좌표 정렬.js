ba95c3eefb13de8cda291997fb777f4c7097b8ac
sun2598 <cksun2598@gmail.com>
2024-05-08 08:57:33 +0900

    24/05/08 solved 07_07

--- section07_정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬.js
+++ section07_정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬.js
@@ -1,5 +1,16 @@
+// N개의 평면상의 좌표(x, y)가 주어지면 모든 좌표를 오름차순으로 정렬하는 프로그램을 작성하세요. 
+// 정렬기준은 먼저 x값에 의해서 정렬하고, x값이 같을 경우 y값에 의해 정렬합니다.
+
 function solution(arr) {
     let answer = arr;
+
+    arr.sort((a, b) => {
+        if (a[0] === b[0]) { // a와 b의 x 좌표가 같다면,
+            return a[1] - b[1]; // y 좌표에 따라 정렬 (오름차순)
+        } else {
+            return a[0] - b[0]; // x 좌표가 다르면 x 좌표에 따라 정렬 (오름차순)
+        }
+    });
 
     return answer;
 }
